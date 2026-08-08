c769267244480f367300ca6d4d8b21bd3552f6f8
AmeyPosane <ameyposane18@gmail.com>
2026-08-08 23:09:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from models import Plant
-from dp import supabase
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from models import Plant
+from dp import supabase
 
 app = FastAPI()
 
@@ -10,80 +11,53 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 @app.get("/")
 def home():
-    return {"message": "My Plant Nursery Project"}
+    return FileResponse("index.html")
 
-
-# Get all plants
 @app.get("/plants")
 def get_plants():
-    data = supabase.table("plants").select("*").execute()
-    return data.data
-    
-# Add plant
+    return supabase.table("plants").select("*").execute().data
+
 @app.post("/plants")
 def add_plant(plant: Plant):
-
     supabase.table("plants").insert({
         "name": plant.name,
         "price": plant.price,
         "quantity": plant.quantity
     }).execute()
-
     return {"message": "Plant Added Successfully"}
 
-
-# Low stock
 @app.get("/low-stock")
 def low_stock():
+    return supabase.table("plants").select("*").lt("quantity", 6).execute().data
 
-    data = supabase.table("plants").select("*").lt("quantity", 6).execute()
-
-    return data.data
-
-
-# Delete plant
 @app.delete("/plants/{plant_name}")
 def delete_plant(plant_name: str):
-
     supabase.table("plants").delete().eq("name", plant_name).execute()
-
     return {"message": "Plant Deleted Successfully"}
 
-
-# Update price
 @app.put("/plants/{plant_name}")
 def update_price(plant_name: str, new_price: int):
-
     supabase.table("plants").update({
         "price": new_price
     }).eq("name", plant_name).execute()
-
     return {"message": "Price Updated Successfully"}
 
-
-# Update quantity
 @app.put("/plants/quantity/{plant_name}")
 def update_quantity(plant_name: str, new_quantity: int):
-
     supabase.table("plants").update({
         "quantity": new_quantity
     }).eq("name", plant_name).execute()
-
     return {"message": "Quantity Updated Successfully"}
 
-
-# Search plant
 @app.get("/search/{name}")
 def search(name: str):
+    return supabase.table("plants").select("*").eq("name", name).execute().data
 
-    data = supabase.table("plants").select("*").eq("name", name).execute()
-
-    return data.data
 @app.post("/orders")
 def create_order(
     customer_name: str,
@@ -97,11 +71,8 @@ def create_order(
         "quantity": quantity,
         "total_price": total_price
     }).execute()
-
     return {"message": "Order Placed Successfully"}
-
 
 @app.get("/orders")
 def get_orders():
-    data = supabase.table("orders").select("*").execute()
-    return data.data
+    return supabase.table("orders").select("*").execute().data
