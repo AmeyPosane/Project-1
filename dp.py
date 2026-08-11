475b06bc284dcbdb7fca237132a66df11f7b055c
AmeyPosane <ameyposane18@gmail.com>
2026-08-11 15:11:39 +0530

    Update dp.py

--- dp.py
+++ dp.py
@@ -1,8 +1,6 @@
-
-import os
 from supabase import create_client
 
-url = os.getenv("https://asyxmwjaouffiqkueyvy.supabase.co")
-key = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzeXhtd2phb3VmZmlxa3VleXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzI0NjAsImV4cCI6MjEwMDY0ODQ2MH0.t4wEUefv7kq0GSGdqtJRnQKJSFibseyBLCgel3FwLR0")
+url = "https://asyxmwjaouffiqkueyvy.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzeXhtd2phb3VmZmlxa3VleXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzI0NjAsImV4cCI6MjEwMDY0ODQ2MH0.t4wEUefv7kq0GSGdqtJRnQKJSFibseyBLCgel3FwLR0"
 
 supabase = create_client(url, key)
